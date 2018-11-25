20e084580383d476d62c1017bdde485192f491e6
eastree <liuding103@163.com>
2018-11-25 13:29:23 +0800

    Initial Commit

---
+++ Inphoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inphoto
+//
+//  Created by liuding on 2018/11/25.
+//  Copyright © 2018 eastree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ InphotoTests/InphotoTests.swift
@@ -0,0 +1,34 @@
+//
+//  InphotoTests.swift
+//  InphotoTests
+//
+//  Created by liuding on 2018/11/25.
+//  Copyright © 2018 eastree. All rights reserved.
+//
+
+import XCTest
+@testable import Inphoto
+
+class InphotoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
